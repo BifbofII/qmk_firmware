7398977735aef71c697c3ede2db5935203a7d3ae
Christoph Jabs <christoph@jabs-family.de>
2020-06-04 09:19:13 +0200

    Bugfix in layout

--- keyboards/kyria/keymaps/bifbofii/keymap.c
+++ keyboards/kyria/keymaps/bifbofii/keymap.c
@@ -68,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *                        `----------------------------------'  `----------------------------------'
      */
     [_QWERTY] = LAYOUT(
-        KC_TAB,               KC_Q, KC_W, KC_E,    KC_R,           KC_T,                                                                                 KC_Y,            KC_U,           KC_I,     KC_O,   KC_P,    KC_PIPE,
+        KC_TAB,               KC_Q, KC_W, KC_E,    KC_R,           KC_T,                                                                                 KC_Y,            KC_U,           KC_I,     KC_O,   KC_P,    KC_BSLS,
         LT(_UNICODE, KC_ESC), KC_A, KC_S, KC_D,    KC_F,           KC_G,                                                                                 KC_H,            KC_J,           KC_K,     KC_L,   KC_SCLN, KC_QUOT,
         KC_LSFT,              KC_Z, KC_X, KC_C,    KC_V,           KC_B,           MO(_LOWER),        MO(_RAISE),     MO(_LOWER),     MO(_RAISE),        KC_N,            KC_M,           KC_COMM,  KC_DOT, KC_SLSH, KC_MINS,
                                           KC_LALT, LCTL_T(KC_TAB), LGUI_T(KC_ENT), LT(_FUNC, KC_SPC), LSFT_T(KC_ESC), RSFT_T(KC_ENT), LT(_FUNC, KC_SPC), RGUI_T(KC_BSPC), RCTL_T(KC_DEL), KC_RALT
