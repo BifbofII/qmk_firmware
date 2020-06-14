65eea061238f2cb0810383a2396def6419d4f8b9
Christoph Jabs <christoph@jabs-family.de>
2020-06-14 15:32:44 +0200

    Added custom split transport
    Only a copy of the common implementation for now, but will be used for
    implementing HID OLEDs

--- keyboards/kyria/keymaps/bifbofii/rules.mk
+++ keyboards/kyria/keymaps/bifbofii/rules.mk
@@ -2,3 +2,8 @@ OLED_DRIVER_ENABLE = yes   # Enables the use of OLED displays
 ENCODER_ENABLE = no        # ENables the use of one or more encoders
 RGBLIGHT_ENABLE = no       # Enable keyboard RGB underglow
 UNICODEMAP_ENABLE = yes
+
+SPLIT_TRANSPORT = custom
+
+SRC += transport.c \
+	serial.c
